63016052f1ef6be67c67ae3f032e474e70d3a952
tiago vieira <t.vieira333@gmail.com>
2024-09-13 12:57:00 -0300

    added metas abertas

--- index.js
+++ index.js
@@ -56,8 +56,23 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) =>{
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)")
+    }
+
+    await select({
+        message: "Metas abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -84,6 +99,11 @@ const start = async () => {
             },
 
             {
+                name: "Metas abertas",
+                value: "abertas"
+            },
+
+            {
                 name: "Sair",
                 value: "sair"
             }
@@ -100,6 +120,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!")
